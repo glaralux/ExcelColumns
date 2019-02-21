d2285276d5ac6cfbf8cfe7c8037aadb9dfec5f12
Gerson <Gerson@LAPTOP-G4AL3JHD>
2019-02-21 01:00:53 +0100

    excelcolumnas

---
+++ excelColumnas/src/excelcolumnas/ExcelColumnas.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excelcolumnas;
+
+/**
+ *
+ * @author Gerson
+ */
+public class ExcelColumnas {
+
+   
+
+
+
+    /**
+
+     * @param args the command line arguments
+
+     */
+
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+
+        char[] nombres = ExcelColumnasnombre(100);
+
+    
+
+    for(char nombre : nombres)
+
+    {
+
+    	System.out.print(nombre + " ");
+
+    }
+
+    }
+
+    
+
+    public static char[] ExcelColumnasnombre(int columnName)
+
+  {
+
+  	
+
+   String[] letras = new String[columnName];
+
+    char[] cadena = new char[columnName];
+
+    char[] cadena2 = new char[columnName];
+
+    int l=65;
+
+    int contador=0;
+
+    	for (int i=0; i<columnName;i++)
+
+        {
+
+        	//letras[i] = (String)('A'+i);
+
+          	cadena[i] = (char)(l+contador);
+
+          contador++;
+
+          if(contador%27==0)
+
+          {
+
+            contador=0;
+
+            cadena[i] = (char) ((char)(l+contador)+(char)(l+contador));
+
+            
+
+            
+
+          }
+
+          
+
+        }
+
+    
+
+        
+
+      return cadena;
+
+    
+
+  }
+
+ 
+
+    
+
+    
+
+}
